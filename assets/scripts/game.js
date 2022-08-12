b0aeef4e7d1fc0f090c85c90398f696ab2452084
admin <rkotelnykov@gmail.com>
2022-08-12 17:05:07 +0300

    Adding Cards

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,11 +6,48 @@ scene.preload = function () {
   // 1. load background
   const background = require('../sprites/background.jpg');
   this.load.image('bg', background); // key (file name), path
+  // 2. Load back part of card
+  const cardBack = require('../sprites/card.png');
+  this.load.image('card', cardBack);
 };
 
 scene.create = function () {
-  this.add.sprite(0, 0, 'bg'); // Canvas coord X, Canvas coord Y, key (from preloader)
-  // 2. Display backgound on Canvas
+  // Display backgound on Canvas
+  this.add.sprite(0, 0, 'bg').setOrigin(0, 0); // Canvas coord X, Canvas coord Y, key (from preloader)
+
+  const positions = this.getCardPositions();
+
+  for (let el of positions) {
+    this.add.sprite(el.x, el.y, 'card').setOrigin(0, 0); // Canvas coord X, Canvas coord Y, key (from preloader)
+  }
+
+  console.log(this);
+};
+
+scene.getCardPositions = function () {
+  const { width, height, rows, cols } = config;
+
+  const cardTexture = this.textures.get('card').getSourceImage();
+  const margin = 5;
+
+  const cardWidth = cardTexture.width + margin;
+  const cardHight = cardTexture.height + margin;
+
+  const offsetX = (width - cardWidth * cols) / 2;
+  const offsetY = (height - cardHight * rows) / 2;
+
+  const positions = [];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      positions.push({
+        x: offsetX + cardWidth * j,
+        y: offsetY + cardHight * i,
+      });
+    }
+  }
+
+  return positions;
 };
 
 const config = {
@@ -18,6 +55,8 @@ const config = {
   width: 1280, // width of Canvas (should take from background size)
   height: 720, // height of Canvas
   scene: scene, // game scene
+  rows: 2, // amount of rows
+  cols: 5, // amount of cols
 };
 
 const game = new Phaser.Game(config);
